f3521a339d3a28f1e9e6cfc75e506cd839eb8feb
Matt <M.Budd@soton.ac.uk>
2023-06-15 12:16:44 +0100

    Creating a Controllers section available purely for testing.

--- src/Tests/Controller/ContentNegotiationController.php
+++ src/Tests/Controller/ContentNegotiationController.php
@@ -1,38 +1,34 @@
 <?php
 
-namespace App\Controller;
+namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use OpenApi\Annotations as OA;
 use App\Controller\Tangerine;
 
 #[Route('/api', name: 'api_')]
 class ContentNegotiationController extends AbstractController
 {
-    /**
-     * @OA\Tag(name="Pirates")
-     */
-    #[Route('/pirates', name: 'pirates_index', methods: ["GET"])]
+    #[Route('/pirates', name: 'pirates_index', methods: ['GET'])]
     public function bar(): Response
     {
 
         $data = [];
         $ids = [0, 1, 2, 3, 4];
-        $names = ["Long John", "Cockle Jim", "Barnacle Bob", "Doubloon Doug", "Seasick Steve"];
+        $names = ['Long John', 'Cockle Jim', 'Barnacle Bob', 'Doubloon Doug', 'Seasick Steve'];
         $descriptions = [
-            "Can drift for days",
-            "Treasure finder",
-            "Stuck to the ship",
-            "A rare gem",
-            "Always in the drink"
+            'Can drift for days',
+            'Treasure finder',
+            'Stuck to the ship',
+            'A rare gem',
+            'Always in the drink'
         ];
 
         foreach ($ids as $id) {
             $data[] = [
                 'id' => $id,
-                'ref' => "PRT",
+                'ref' => 'PRT',
                 'name' => $names[$id],
                 'description' => $descriptions[$id]
             ];
@@ -40,20 +36,20 @@ class ContentNegotiationController extends AbstractController
         return $this->json($data);
     }
 
-    #[Route('/pirates/text', name: 'pirates_text', methods: ["GET"])]
+    #[Route('/pirates/text', name: 'pirates_text', methods: ['GET'])]
     public function boo(): Response
     {
 
-        $textResponse = new Response("Pirates!", 200);
+        $textResponse = new Response('Pirates!', 200);
         $textResponse->headers->set('Content-Type', 'text/plain');
 
         return $textResponse;
     }
 
-    #[Route('/five-hundred', name: 'five_index', methods: ["POST"])]
+    #[Route('/five-hundred', name: 'five_index', methods: ['POST'])]
     public function fiveHundred(): Response
     {
-        $tangerine = "mandarins";
+        $tangerine = 'mandarins';
 
         throw new Tangerine($tangerine);
     }
