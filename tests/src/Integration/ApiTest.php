eb920b77824debeeab80d8086d85208ca60e7db3
Matt <M.Budd@soton.ac.uk>
2023-06-21 16:58:35 +0100

    Reverting test structure.

--- tests/src/Integration/ApiTest.php
+++ tests/src/Integration/ApiTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Tests\Integration;
+namespace src\Integration;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
--- tests/src/Integration/Controller/ContentNegotiationControllerTest.php
+++ tests/src/Integration/Controller/ContentNegotiationControllerTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Tests\Integration\Controller;
+namespace src\Integration\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
