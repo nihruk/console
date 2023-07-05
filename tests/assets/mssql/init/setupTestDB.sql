a310cb74c729671a28b34b7afab13c71af8713f2
bernard <b.roper@soton.ac.uk>
2023-07-05 09:28:51 +0100

    db initialisation sql to set up user and login

--- tests/assets/mssql/init/setupTestDB.sql
+++ tests/assets/mssql/init/setupTestDB.sql
@@ -13,3 +13,19 @@ Occupation  VARCHAR(40)
     VALUES ('Bob', 'Builder'), ('Lara', 'Tomb Raider'), ('Pat', 'Postman'), ('Dora', 'Explorer' )
 
 GO
+
+USE [master]
+GO
+CREATE LOGIN [ioda] WITH PASSWORD=N'g4t3sC4n34tMySh0rtz', DEFAULT_DATABASE=[infonihr]
+ALTER SERVER ROLE [dbcreator] ADD MEMBER [ioda]
+GO
+
+USE [infonihr]
+GO
+CREATE USER [ioda] FOR LOGIN [ioda] WITH DEFAULT_SCHEMA=[dbo]
+GO
+
+USE [infonihr]
+GO
+ALTER ROLE [db_owner] ADD MEMBER [ioda]
+GO
