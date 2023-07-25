8377f41d7cf1a74eadee42aba89acb3592f6d2a4
Matt <M.Budd@soton.ac.uk>
2023-07-25 10:34:00 +0100

    Updates #2

--- tests/src/Integration/Controller/LuceneControllerTest.php
+++ tests/src/Integration/Controller/LuceneControllerTest.php
@@ -8,26 +8,6 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class LuceneControllerTest extends WebTestCase
 {
-    public function testLuceneParser(): void
-    {
-        $test_file = (string)file_get_contents('/srv/ioda/tests/config/testLuceneParser.json');
-        $test_data = (array)json_decode($test_file, true);
-        $test_query = is_string($test_data['query']) ? $test_data['query'] : '';
-        $test_response = is_string($test_data['response']) ? $test_data['response'] : '';
-        $client = static::createClient();
-        $client->request(
-            'GET',
-            '/api/parse/' . $test_query,
-        );
-        $this->assertResponseIsSuccessful();
-        $this->assertResponseHeaderSame('Content-Type', 'application/json');
-        $response = $client->getResponse();
-        $this->assertJson((string)$response->getContent());
-        $this->assertJsonStringEqualsJsonString(
-            $test_response,
-            (string)$response->getContent()
-        );
-    }
 
     public function testElastic(): void
     {
