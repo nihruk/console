00f83ce87fa4b31bf17911494f8f98a980c8a21c
Matt <M.Budd@soton.ac.uk>
2023-07-26 14:59:01 +0100

    Moving setup around in order to fulfill the new no 'build' commands in compose file rule.

---
+++ docker/logstash/entrypoint.sh
@@ -0,0 +1,6 @@
+apt-get install apt-utils
+apt-get install wget
+wget https://github.com/microsoft/mssql-jdbc/releases/download/v12.2.0/mssql-jdbc-12.2.0.jre8.jar -O mssql-jdbc-12.2.0.jre8.jar
+mv /usr/share/logstash/mssql-jdbc-12.2.0.jre8.jar /usr/share/logstash/drv/mssql-jdbc-12.2.0.jre8.jar
+chmod 0444 /usr/share/logstash/drv/mssql-jdbc-12.2.0.jre8.jar
+logstash
